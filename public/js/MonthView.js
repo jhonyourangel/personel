2e7ab069ee6a6cbb33e3061e58eab5e670182781
jhonyourangel <alexionutale@me.com>
2017-05-30 04:27:02 +0200

    features
    added the time stamp to the rowOBJ
    function to convert to MS
    function to convert to String
    function to calculate worked hours with math round up and down
    the wh are calculated in real time so u ca see how much you actually
    worked in that specific day
    
    added var thisMonthDate
    
    removed old vars that were no longer in use

--- public/js/MonthView.js
+++ public/js/MonthView.js
@@ -7,7 +7,7 @@ Date.prototype.addDays = function(d) {
     return this.valueOf() + 864E5 * d;
 };
 Date.prototype.getMonthName = function() {
-    var monthArray = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec']
+    let monthArray = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec']
     return monthArray[this.getMonth()];
 }
 Date.prototype.getLastDAyOfMonth = function(month) {
@@ -17,20 +17,10 @@ Date.prototype.getLastDAyOfMonth = function(month) {
     return new Date(this.getFullYear(), month + 1, 0).getDate();
 }
 
-var sqares = 100;
-var todayDate = new Date();
-var container = document.getElementById('myRows');
-
-
-var cRect = container.getBoundingClientRect();
-var cWidth = 500;
-var cHeight = 500;
-var cardHeight = 255;
-var cardWidth = 200;
-var sep = 10;
-var cTop = cRect.top;
-var topPos = (cHeight + (cardHeight * 2)) - cTop;
-var bottomPos = (cHeight + (cardHeight * 2)) - cTop;
+let sqares = 100;
+let todayDate = new Date();
+let thisMonthDate = new Date(todayDate.getFullYear(), todayDate.getMonth(), 1);
+let container = document.getElementById('myRows');
 
 //*****************************************************
 //*************** scroll event
@@ -40,33 +30,39 @@ var bottomPos = (cHeight + (cardHeight * 2)) - cTop;
 // the css attribute "position" : "absolute" is positioning all the 
 // elements on the first pixels of the .container
 
-var objArr = [];
+let objArr = [];
 
 function nrOfRows() {
     //return Math.round(cHeight / (sep + cardHeight));
-    return todayDate.getLastDAyOfMonth()
+    return thisMonthDate.getLastDAyOfMonth()
 }
 
 
 
 function render() {
-    var genHtml = ""
+    let genHtml = ""
     console.log(nrOfRows());
-    for (var i = 0, max = nrOfRows(); i < max; i++) {
-        var rowO = new RowModel();
+    for (let i = 0, max = nrOfRows(); i < max; i++) {
+        let rowO = new RowModel();
         rowO.id = i;
-        rowO.index = i;
-        rowO.setText(formatDate(todayDate.addDays(rowO.index)));
-        rowO.vPos = (cardHeight + sep) * i + cTop;
+        rowO.setText(formatDate(thisMonthDate.addDays(rowO.id)));
+
         genHtml += rowO.htmlString();
         objArr.push(rowO);
-
     }
     $('#myRows').append(genHtml);
+    _BindObjArr();
 };
 
-var RowModel = function() {
+let RowModel = function() {
     this.text = ""
+    this.id = 0;
+    this.timeStamp = {
+        in_1: "01:00",
+        out_1: "00:00",
+        in_2: "00:00",
+        out_2: "00:00"
+    }
     this.setText = function(value) {
         this.text = value;
         return "text " + this.text;
@@ -79,21 +75,22 @@ var RowModel = function() {
         <h5><span class="fa fa-calendar"></span> ${this.text} </h5>
         <div class="input-group">
             <span class="input-group-addon" id="basic-addon1"><span class="fa fa-sign-in"></span></span>
-            <input type="number" class="form-control" placeholder="00/00" aria-describedby="basic-addon1">
+            <input id="${this.id}_1_in" type="time" class="form-control" value="00:00" aria-describedby="basic-addon1">
         </div>
         <div class="input-group">
             <span class="input-group-addon" id="basic-addon1"><span class="fa fa-sign-out"></span></span>
-            <input type="number" class="form-control" placeholder="00/00" aria-describedby="basic-addon1">
+            <input id="${this.id}_1_out" type="time" class="form-control" value="00:00" aria-describedby="basic-addon1">
         </div>
         <br>
         <div class="input-group">
             <span class="input-group-addon" id="basic-addon1"><span class="fa fa-sign-in"></span></span>
-            <input type="number" class="form-control" placeholder="00/00" aria-describedby="basic-addon1">
+            <input id="${this.id}_2_in" type="time" class="form-control" value="00:00" aria-describedby="basic-addon1">
         </div>
         <div class="input-group">
             <span class="input-group-addon" id="basic-addon1"><span class="fa fa-sign-out"></span></span>
-            <input type="number" class="form-control" placeholder="00/00" aria-describedby="basic-addon1">
+            <input id="${this.id}_2_out" type="time" class="form-control" value="00:00" aria-describedby="basic-addon1">
         </div>
+        <h6 id="${this.id}_wh">08:00</h6>
     </div>
 </div>`;
 
@@ -101,8 +98,66 @@ var RowModel = function() {
 };
 
 function formatDate(dateVal) {
-    var tempDate = new Date(dateVal);
-    var monthArray = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec']
+    let tempDate = new Date(dateVal);
+    let monthArray = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec']
     return `${tempDate.getDate()} ${monthArray[tempDate.getMonth()]} ${tempDate.getFullYear()}`;
 }
+
+function bindModelInput(obj, property, domElem) {
+    Object.defineProperty(obj, property, {
+        get: function() {
+            return domElem.value;
+        },
+        set: function(newValue) {
+            domElem.value = newValue;
+        },
+        configurable: true
+    });
+}
+
+function _BindObjArr() {
+    for (let row of objArr) {
+        bindModelInput(row.timeStamp, 'in_1', document.getElementById(row.id + "_1_in"));
+        bindModelInput(row.timeStamp, 'out_1', document.getElementById(row.id + "_1_out"));
+        bindModelInput(row.timeStamp, 'in_2', document.getElementById(row.id + "_2_in"));
+        bindModelInput(row.timeStamp, 'out_2', document.getElementById(row.id + "_2_out"));
+    }
+    $('input').change(function() {
+        console.log(`the ${this.id} changed the value to ${this.value}`);
+        let thisID = this.id.split('_')[0];
+
+        let in1 = objArr[thisID].timeStamp.in_1;
+        let out1 = objArr[thisID].timeStamp.out_1;
+        let in2 = objArr[thisID].timeStamp.in_2;
+        let out2 = objArr[thisID].timeStamp.out_2;
+
+        var workedHours = calculateWorkedTime(convertToMS(in1), convertToMS(out1), convertToMS(in2), convertToMS(out2));
+        let whMS = convertToStringTime(workedHours);
+
+        $(`#${thisID}_wh`).text(whMS);
+    });
+}
+
+function convertToMS(string) {
+    let m = 1000 * 60;
+    let h = m * 60;
+    let timeArr = string.split(":");
+
+    return (timeArr[0] * h) + (timeArr[1] * m)
+}
+
+function convertToStringTime(ms) {
+    let x = ms / 1000
+    let seconds = x % 60
+    x /= 60
+    let minutes = x % 60
+    x /= 60
+    let hours = x % 24
+    return `${Math.floor(hours)}:${Math.round(minutes)}`;
+}
+
+function calculateWorkedTime(in_1, out_1, in_2, out_2) {
+    return (out_2 - in_2) + (out_1 - in_1);
+}
+
 render();
