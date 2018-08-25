9818869d52d0e734956c81dac08464c340b2c354
ion utale <ion.utale@icloud.com>
2018-08-25 11:35:18 +0200

    added redirection to https and a small fix on ui for iPhone 5

--- app.js
+++ app.js
@@ -38,6 +38,14 @@ app.use(express.static(path.join(__dirname, 'app_client/dist')));
 // [SH] Initialise Passport before using the route middleware
 app.use(passport.initialize());
 
+// set up a route to redirect http to https
+app.get('*', function(req, res) {  
+    res.redirect('https://' + req.headers.host + req.url);
+
+    // Or, if you don't want to automatically detect the domain name from the request header, you can hard code it:
+    // res.redirect('https://example.com' + req.url);
+})
+
 // [SH] Use the API routes when path starts with /api
 app.use('/api', routesApi);
 
